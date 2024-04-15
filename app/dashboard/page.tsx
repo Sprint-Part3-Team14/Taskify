4cc9012685930d9f9c158a29e4a969363785b33b
KKH <gyooheon@naver.com>
2024-04-15 16:00:35 +0900

    💄 Add : add a vertical divider
    dashboard 리스트에 세로 구분선 생성

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,11 +21,11 @@ const Dashboard = () => {
     <section className='flex min-h-screen  '>
       <div className='flex min-h-full min-w-full w-full '>
         <div className='w-72 bg-slate-500'>사이드바 임시 공간...</div>
-        <div className='w-full'>
+        <div className='w-full '>
           <div className=' h-20 bg-slate-400'>헤더 임시 공간 ...</div>
           <div className='flex '>
             {cardlists.map((list, index) => (
-              <DashboardList key={index} title={list.title} cardData={list.cards} />
+              <DashboardList key={index} title={list.title} cardDatas={list.cards} />
             ))}
             <div className='w-fit grow py-16 px-5'>
               <AddButton />
