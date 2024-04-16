548687e84090f35f496e00f1d8e948d3d869cb9c
codingaring <sksms2104@naver.com>
2024-04-17 00:24:08 +0900

    ✨ Feat : add CreateWorkModal

--- app/modal/page.tsx
+++ app/modal/page.tsx
@@ -2,9 +2,11 @@
 
 import ModalButton from '@components/Modal/Button/ModalButton';
 import CreateColumnModal from '@components/Modal/CreateColumnModal';
+import CreateWorkModal from '@components/Modal/CreateWorkModal';
 import DeleteAllCardModal from '@components/Modal/DeleteAllCardModal';
 import EditColumnModal from '@components/Modal/EditColumnModal';
 import ModalDropdown from '@components/Modal/Input/ModalDropdown';
+import ModalTextarea from '@components/Modal/Input/ModalTextarea';
 import InviteModal from '@components/Modal/InviteModal';
 import WrongPasswordModal from '@components/Modal/WrongPasswordModal';
 
@@ -13,6 +15,8 @@ const modalTest = () => {
     <>
       <ModalDropdown title='상태' />
       <ModalButton buttonType='single' singleButton='입력' />
+      <ModalTextarea />
+      <CreateWorkModal />
     </>
   );
 };
