fe20100013147e0e3716418dacb42eb4830cc7e0
Taeuk471 <asky990223@gmail.com>
2024-04-16 23:50:20 +0900

    :sparkles: Feat : make SettingSection file

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,6 +6,7 @@ import Article from '@/components/landing/article';
 import Footer from '@/components/landing/footer';
 import Header from '@/components/landing/header';
 import MainSection from '@/components/landing/mainSection';
+import SettingSection from '@/components/landing/settingSection';
 // import ModalDescription from '@/components/landing/modalDescription';
 import SubArticle from '@/components/landing/subarticle';
 
@@ -26,8 +27,8 @@ const Home = () => {
         <MainSection />
         <Article />
         <SubArticle />
-        <p className='text-20 font-bold tablet:text-[36px]'>생산성을 높이는 다양한 설정 ⚡</p>
-        {/* <ModalDescription /> */}
+        <p className='flex justify-center text-20 font-bold tb:text-[36px]'>생산성을 높이는 다양한 설정 ⚡</p>
+        <SettingSection />
         <Footer />
       </div>
     </RootLayout>
--- components/landing/subarticle.tsx
+++ components/landing/subarticle.tsx
@@ -7,7 +7,7 @@ const SubArticle = () => {
     <div className='flex justify-center'>
       <div className='flex h-[42.875rem] w-11/12 min-w-fit max-w-7xl flex-col justify-center overflow-hidden rounded-md text-center tb:h-[60.75rem] tb:justify-start pc:grid pc:h-[37.5rem] pc:w-4/6 pc:grid-cols-2 pc:justify-items-center bg-violet-100'>
         <div className='flex-center pc:h-[38.75rem] pc:w-[37.125rem]'>
-          <div className='relative mt-22 flex h-250 w-217 tablet:mt-120 tablet:h-415 tablet:w-360 pc:h-[31.25rem] pc:w-436 pc:mt-[6.25rem] pc:ml-[4rem] pc:mr-[3rem]'>
+          <div className='relative mt-22 flex h-250 w-217 tb:mt-120 tb:h-415 tb:w-360 pc:h-[31.25rem] pc:w-436 pc:mt-[6.25rem] pc:ml-[5rem] pc:mr-[4rem]'>
             <Image src={DashBoardImage} alt='Illustration' fill />
           </div>
         </div>
