bba08c2ec59abc5662a86adb990bc38b31254711
KKH <gyooheon@naver.com>
2024-04-17 13:53:40 +0900

    ✨ Feat : make the add cards function
    카드 데이터를 받아서 새로운 카드를 만드는 기능 구현

--- components/dashboard/DashboardList/DashboardList.tsx
+++ components/dashboard/DashboardList/DashboardList.tsx
@@ -1,33 +1,37 @@
-import { usePathname } from 'next/navigation';
+'use client';
+
+import { useState, useEffect } from 'react';
 
 import AddButton from '../AddButton/AddButton';
 import DashboardTitle from '../DashboardTitle/DashboardTitle';
 import TempCard from '../TempCard';
 
 import Card from '@/components/Card/Card';
+import { I_Card } from '@/interface/Card';
 import { I_CardList } from '@/interface/Dashboard';
 
 const DashboardList = ({ cardDatas, title }: I_CardList) => {
-  const path = usePathname();
-  const dashboardId = path.split('/')[2];
+  const [newCard, setNewCard] = useState<I_Card[]>([]);
+  const [newCardList, setnewCardList] = useState<I_Card[]>([]);
 
-  console.log(dashboardId);
+  useEffect(() => {
+    const addCard = [...(cardDatas || []), ...newCard];
+    setnewCardList(addCard);
+  }, [newCard, cardDatas]);
+
+  const handleNewCard = (addCard: I_Card[]) => {
+    setNewCard(prevNewCard => [...prevNewCard, ...addCard]);
+  };
+
+  console.log(newCard);
 
   return (
     <div className='flex flex-col border border-r-[1px] border-dotted  gap-4 w-96  min-h-screen p-5 '>
       <DashboardTitle title={title} count={cardDatas?.length || 0} />
       <AddButton />
-      <TempCard />
-      {cardDatas?.map((card, index) => (
-        <Card
-          key={index}
-          title={card.title}
-          image={card.image}
-          alt={card.title}
-          tag={card.tag}
-          date={card.date}
-          user={card.user}
-        />
+      <TempCard onChange={handleNewCard} />
+      {newCardList?.map(({ title, image, tag, date, user }, index) => (
+        <Card key={index} title={title} image={image} alt={title} tag={tag} date={date} user={user} />
       ))}
     </div>
   );
--- components/dashboard/TempCard.tsx
+++ components/dashboard/TempCard.tsx
@@ -1,12 +1,30 @@
-const TempCard = () => {
+import NumberChip from '../Chip/NumberChip';
+import TagChip from '../Chip/TagChip';
+
+import { I_Card } from '@/interface/Card';
+
+interface Props {
+  onChange: (newCard: I_Card[]) => void;
+}
+
+const TempCard = ({ onChange }: Props) => {
+  const addCard: I_Card = {
+    title: 'Task 1',
+    date: '2024-04-13',
+    tag: [<TagChip key={1} name='백엔드' size='large' color='red' />],
+    user: <NumberChip count={5} />,
+  };
+
+  const handleAddCard = () => {
+    onChange([addCard]);
+  };
+
   return (
     <div>
       <div>카드생성</div>
-      <input className='border border-solid border-red-400 ' placeholder='user' />
-      <input className='border border-solid border-red-400 ' placeholder='title' />
-      <input className='border border-solid border-red-400 ' placeholder='date' />
-      <input className='border border-solid border-red-400 ' placeholder='tag' />
-      <button className='bg-teal-300'>제출</button>
+      <button onClick={handleAddCard} className='bg-teal-300'>
+        제출
+      </button>
     </div>
   );
 };
