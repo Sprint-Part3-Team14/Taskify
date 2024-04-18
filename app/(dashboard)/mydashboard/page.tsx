131a65a40f19bf64803716a17c1add3bcb8bb1ab
KKH <gyooheon@naver.com>
2024-04-18 11:26:52 +0900

    ✨ Feat : make a mydashobard my list component
    mydashboard페이지 내가 이용하고 있는 대시보드 리스트 확인할 수 있는 부분 컴포넌트화 하여 제작

--- app/(dashboard)/mydashboard/page.tsx
+++ app/(dashboard)/mydashboard/page.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
+import MyList from '@/components/mydashboard/MyList/MyList';
+import SEARCH_ICON from '@/public/icon/search.svg';
+
 const Mydashboard = () => {
-  return <div>make mydashboard UI</div>;
+  return (
+    <div className=' flex flex-col w-[1024px] gap-11 px-10 pt-10 '>
+      <MyList />
+      <div className='flex flex-col gap-6 w-full px-7 py-8'>
+        <div className='text-2xl font-bold'>초대 받은 대시보드</div>
+        <div className='flex w-full px-4 py-2 gap-2 border border-solid rounded-md border-tp-gray_700'>
+          <Image src={SEARCH_ICON} alt='search' width={24} height={24} />
+          <input className='w-full text-base placeholder:text-tp-gray_800' placeholder='검색' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mydashboard;
