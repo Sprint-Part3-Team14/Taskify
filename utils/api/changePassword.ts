caf93641509c54c428cb455e17f9b53ea91675e9
codingaring <sksms2104@naver.com>
2024-04-19 13:52:49 +0900

    ✨Feat : add ChangePassword API function

---
+++ utils/api/changePassword.ts
@@ -0,0 +1,17 @@
+import { ACCESS_TOKEN } from '../temporaryToken';
+
+export interface PasswordData {
+  password : string;
+  newPassword : string;
+}
+
+export const changePassWord = async ({changePasswordValue} : {changePasswordValue : PasswordData} ) => {
+  const response = await fetch(`https://sp-taskify-api.vercel.app/14/auth/password`, {
+    method: 'PUT',
+    body : JSON.stringify(changePasswordValue),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: ACCESS_TOKEN,
+    },
+  });
+};
