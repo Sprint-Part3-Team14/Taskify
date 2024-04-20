768fbbb1f5f4bde7f985f81ef4db29587061cfa1
Taeuk471 <asky990223@gmail.com>
2024-04-20 19:03:49 +0900

    :sparkles: Feat : add Vaild ErrorMessage logic

---
+++ constant/errorMessage.ts
@@ -0,0 +1,21 @@
+export const ERROR_MESSAGES = {
+  email: {
+    field: '이메일을 입력해주세요.',
+    fattern: '이메일 형식으로 작성해 주세요.',
+    verify: '이메일을 확인해주세요.',
+  },
+  nickname: {
+    field: '닉네임을 입력해주세요.',
+    pattern: '닉네임은 영어 또는 한글을 사용해 10자 이하로 작성해주세요.',
+  },
+  password: {
+    field: '비밀번호를 입력해주세요.',
+    pattern: '8자 이상 입력해 주세요.',
+    verify: '비밀번호를 확인해주세요.',
+    invalidNewPassword: '기존 비밀번호과 다른 비밀번호를 입력해주세요.',
+  },
+  passwordCh: {
+    field: '비밀번호가 일치하지 않습니다.',
+    newPasswordChField: '새 비밀번호가 일치하지 않습니다.',
+  },
+};
