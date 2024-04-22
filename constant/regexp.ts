4a89e3eeea0c72899565b4c50c71e7658e9d881e
Taeuk471 <asky990223@gmail.com>
2024-04-23 05:07:06 +0900

    :sparkles: Feat : make entire SignUp Page

---
+++ constant/regexp.ts
@@ -0,0 +1,7 @@
+// 정규 표현식
+
+export const REG_EXP = {
+  VALID_EMAIL: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  VALID_NICKNAME: /^[가-힣A-Za-z]{2,10}$/,
+  VALID_PASSWORD: /^[\Sa-zA-Z0-9]{8,}$/,
+};
