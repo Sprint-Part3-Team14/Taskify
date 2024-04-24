4107338746094a8ac1514c00de45f99b4a694ded
codingaring <sksms2104@naver.com>
2024-04-24 15:21:41 +0900

    ✨ Feat : add postInvitation api function

---
+++ utils/api/postInvitation.ts
@@ -0,0 +1,15 @@
+import { ACCESS_TOKEN } from '../temporaryToken';
+
+export const postInvitation = async ({ email, dashboardId }: { email: string; dashboardId: number }) => {
+  const response = await fetch(`https://sp-taskify-api.vercel.app/14/dashboards/${dashboardId}/invitations`, {
+    method: 'POST',
+    body: JSON.stringify(email),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: ACCESS_TOKEN,
+    },
+  });
+
+  const result = response.json();
+  return result;
+};
