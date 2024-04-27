0f2372130ce0e1b3abad61690b294b1af426c4b4
codingaring <sksms2104@naver.com>
2024-04-27 20:50:47 +0900

    ♻️ Refactor : add errorlogic deltePostInvitation.ts

--- utils/api/deletePostInvitation.ts
+++ utils/api/deletePostInvitation.ts
@@ -18,5 +18,19 @@ export const deletePostInvitation = async ({ dashboardId, invitationId }: I_Dele
   );
 
   const result = await response.json();
-  return result;
+
+  switch (response.status) {
+    case 204: {
+      return 'success';
+    }
+    case 403: {
+      throw new Error('대시보드 초대 취소 권한이 없습니다. 다시 로그인해 주세요.');
+    }
+    case 404: {
+      throw new Error('존재하지 않는 초대 내역입니다.');
+    }
+    default: {
+      throw new Error('알 수 없는 이유로 초대 취소에 실패했습니다. 다시 로그인해 주세요.');
+    }
+  }
 };
