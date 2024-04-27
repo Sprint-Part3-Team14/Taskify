a255ca1876139b04a39252dd18884702c4426f5a
codingaring <sksms2104@naver.com>
2024-04-27 20:48:03 +0900

    ♻️ Refactor : add errorlogic deleteDashboard.ts

--- utils/api/deleteDashBoard.ts
+++ utils/api/deleteDashBoard.ts
@@ -1,11 +1,25 @@
 import { ACCESS_TOKEN } from '../temporaryToken';
 
 export const deleteDashBoard = async ({ dashboardId }: { dashboardId: number }) => {
-  await fetch(`https://sp-taskify-api.vercel.app/14/dashboards/${dashboardId}`, {
+  const response = await fetch(`https://sp-taskify-api.vercel.app/14/dashboards/${dashboardId}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
       Authorization: ACCESS_TOKEN,
     },
   });
+  switch (response.status) {
+    case 204: {
+      return 'success';
+    }
+    case 403: {
+      throw new Error('대시보드 삭제 권한이 없습니다. 다시 로그인해 주세요.');
+    }
+    case 404: {
+      throw new Error('존재하지 않는 대시보드입니다.');
+    }
+    default: {
+      throw new Error('알 수 없는 이유로 대시보드 삭제에 실패했습니다. 다시 로그인해 주세요.');
+    }
+  }
 };
