c2ac3197d59b0947c06fca8ba8938c952aa2f1a0
codingaring <sksms2104@naver.com>
2024-04-27 20:57:09 +0900

    ♻️ Refactor : add error logic getLoginUserProfile.ts

--- utils/api/getLoginUserProfile.ts
+++ utils/api/getLoginUserProfile.ts
@@ -9,7 +9,18 @@ export const getLoginUserProfile = async () => {
     },
   });
   const result = response.json();
-  return result;
+  switch (response.status) {
+    case 200: {
+      return result;
+    }
+    case 401: {
+      throw new Error('올바르지 않은 조회입니다. 다시 로그인해 주세요.');
+    }
+    case 404: {
+      throw new Error('존재하지 않는 대시보드입니다.');
+    }
+    default: {
+      throw new Error('알 수 없는 이유로 회원정보 조회에 실패했습니다. 다시 로그인해 주세요.');
+    }
+  }
 };
-
-// 현재 임의로 쿠키 값을 넣어줌
