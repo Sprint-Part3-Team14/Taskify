f703d0d5a2d3e5bf8b658823e958619ad255437e
codingaring <sksms2104@naver.com>
2024-04-27 20:55:33 +0900

    ♻️ Refactor : add error logic getInvitations.ts

--- utils/api/getInvitations.ts
+++ utils/api/getInvitations.ts
@@ -17,5 +17,18 @@ export const getInvitations = async ({ currentPage, showCount, dashboardId }: In
     }
   );
   const result = await response.json();
-  return result;
+  switch (response.status) {
+    case 200: {
+      return result;
+    }
+    case 403: {
+      throw new Error('대시보드 초대 조회 권한이 없습니다.');
+    }
+    case 404: {
+      throw new Error('존재하지 않는 대시보드입니다.');
+    }
+    default: {
+      throw new Error('알 수 없는 이유로 대시보드 초대 조회에 실패했습니다. 다시 로그인해 주세요.');
+    }
+  }
 };
