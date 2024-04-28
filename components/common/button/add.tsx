482a576d154cdcdb947b4e54de9eda1f7583b63f
Taeuk471 <asky990223@gmail.com>
2024-04-28 17:44:15 +0900

    :recycle: Refactor : refactor button props

--- components/common/button/add.tsx
+++ components/common/button/add.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image';
-
-import { I_ADD_BUTTON } from 'interface/button';
 import PLUS_VIOLET from '@/public/images/icon/violet_plus.svg';
 
-const AddButton = ({ children, onClick }: I_ADD_BUTTON) => {
+interface I_ADD_BUTTON {
+  children?: React.ReactNode;
+  onClick: () => void;
+  id?: string;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+const AddButton = ({ children, onClick, id, type = 'button' }: I_ADD_BUTTON) => {
   return (
     <button
+      id={id}
+      type={type}
       className='flex mb:justify-center items-center gap-2.5 text-lg font-bold  tb:min-w-0 w-full h-[4.375rem] py-1 tb:py-2 rounded-md bg-tp-white  border border-solid border-tp-gray_700 hover:bg-tp-gray_500 hover:border-tp-gray_800'
       onClick={onClick}>
       {children}
-      <div className='flex items-center justify-center w-5 h-5 tb:w-6 tb:h-6 p-0.5 bg-tp-violet_100 rounded  '>
+      <div className='flex items-center justify-center w-5 h-5 tb:w-6 tb:h-6 p-0.5 bg-tp-violet_100 rounded'>
         <Image src={PLUS_VIOLET} alt='plus' width={18} height={18} />
       </div>
     </button>
--- components/common/button/remove.tsx
+++ components/common/button/remove.tsx
@@ -1,10 +1,18 @@
-import { PropsWithChildren } from 'react';
-
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import Image from 'next/image';
 
-const RemoveButton = ({ children }: PropsWithChildren) => {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  id?: string;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+const RemoveButton = ({ children, id, type = 'button', ...rest }: PropsWithChildren<Props>) => {
   return (
-    <button className='flex justify-center text-lg font-bold w-[284px] tb:w-[320px] pc:w-[320px] py-5 tb:py-6 rounded-md  border border-solid border-tp-gray_700'>
+    <button
+      id={id}
+      type={type}
+      className='flex justify-center text-lg font-bold w-[284px] tb:w-[320px] pc:w-[320px] py-5 tb:py-6 rounded-md border border-solid border-tp-gray_700'
+      {...rest}>
       {children}
       <div className='flex items-center justify-center'>
         <p className='flex font-bold text-base tb:text-lg'>대시보드 삭제하기</p>
