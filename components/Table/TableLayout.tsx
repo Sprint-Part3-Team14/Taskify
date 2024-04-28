49956a587ecddc3bd21a678b235ebee686b1a479
codingaring <sksms2104@naver.com>
2024-04-28 17:02:44 +0900

    ♻️ Refactor : add error logic and remove css -mb

--- components/Table/TableLayout.tsx
+++ components/Table/TableLayout.tsx
@@ -5,13 +5,13 @@ const TableLayout = ({
 }: {
   title: '구성원' | '초대 내역' | '초대받은 대시보드';
   headerContent?: JSX.Element;
-  tableContent: JSX.Element[];
+  tableContent: JSX.Element;
 }) => {
   return (
-    <div className='flex flex-col rounded-md bg-tp-white shadow-sm pc:pt-8 pc:pb-7 mb:pt-6 pc:w-[38.75rem] tb:w-[34rem] mb:w-[17.75rem] pc:h-[31rem] h-[29rem]'>
-      <div className='flex justify-between pc:px-7 mb:px-5'>
-        <h1 className='font-bold text-tp-black_700 pc:text-[1.25rem] mb:text-xl'>{title}</h1>
-        {headerContent && headerContent}
+    <div className='flex flex-col rounded-md bg-tp-white shadow-sm pc:pt-8 pc:pb-7 pt-6 pc:w-[38.75rem] tb:w-[34rem] w-[19rem] pc:h-[35rem] h-[34rem]'>
+      <div className='flex justify-between pc:px-7 px-5 items-center'>
+        <h1 className='font-bold text-tp-black_700 pc:text-[1.25rem] text-xl'>{title}</h1>
+        {headerContent}
       </div>
       <div className='flex flex-col gap-2'>
         <div className='flex flex-col gap-2.5'>{tableContent}</div>
