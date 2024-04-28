f523eb9ab3f98141d49a858daad4ca6c45e6800a
codingaring <sksms2104@naver.com>
2024-04-28 15:16:26 +0900

    ♻️ Refactor : add error logic getProfile

--- utils/api/changeUserProfile.ts
+++ utils/api/changeUserProfile.ts
@@ -14,6 +14,18 @@ export const changeUserProfile = async ({ newProfileData }: { newProfileData: Ne
       Authorization: ACCESS_TOKEN,
     },
   });
-  const result = response.json();
-  return result;
+
+  if (response.ok) {
+    const result = response.json();
+    return result;
+  }
+
+  switch (response.status) {
+    case 400: {
+      throw new Error('닉네임은 10자 이하로 작성해 주세요.');
+    }
+    case 401: {
+      throw new Error('프로필 변경에 실패했습니다. 다시 로그인해 주세요.');
+    }
+  }
 };
