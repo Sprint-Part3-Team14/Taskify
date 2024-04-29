f665c583d8e54ee90bf55ca8f7e5399dc0e88243
Taeuk471 <asky990223@gmail.com>
2024-04-29 16:41:18 +0900

    :recycle: Refactor image for landing page

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import logo from '@/public/images/logo/logo_large.jpg';
+import { LOGO } from 'constant/importImage';
 
 const Header = () => {
   return (
     <div className='z-nav flex h-70 w-full items-center justify-between bg-white px-12 '>
       <div className='p-4'>
-        <Image src={logo} alt='Logo' width={121} height={39} />
+        <Image src={LOGO} alt='Logo' width={121} height={39} />
       </div>
       <div className='flex gap-5 tb:gap-9'>
         <Link href='/signin'>
--- constant/importImage.ts
+++ constant/importImage.ts
@@ -1,30 +1,30 @@
+import ArrowBackwardIcon from '@/public/images/icon/arrow_backward.svg';
+import EditIcon from '@/public/images/icon/edit.svg';
+import CloseIcon from '@/public/images/icon/close.svg';
+import ArrowForwardIcon from '@/public/images/icon/arrow_forward.svg';
+import ArrowDropDownIcon from '@/public/images/icon/arrow_drop_down.svg';
+import EllipseIcon from '@/public/images/icon/ellipse.svg';
+import CodeitIcon from '@/public/images/icon/codeit.svg';
+import CalendarTodayIcon from '@/public/images/icon/calendar_today.svg';
+import CrownIcon from '@/public/images/icon/crown.svg';
+import CheckIcon from '@/public/images/icon/check.svg';
+import EmailIcon from '@/public/images/icon/email.svg';
+import HiddenEyeIcon from '@/public/images/icon/hidden_eye.svg';
+import SearchIcon from '@/public/images/icon/search.svg';
+import PlusIcon from '@/public/images/icon/plus.svg';
+import PlusVioletIcon from '@/public/images/icon/plus_violet.svg';
+import SettingIcon from '@/public/images/icon/setting.svg';
+import PlusBlueIcon from '@/public/images/icon/plus_blue.svg';
+import InstagramIconIcon from '@/public/images/icon/instagram_icon.svg';
+import FacebookIconIcon from '@/public/images/icon/facebook_icon.svg';
+import MoreVertIcon from '@/public/images/icon/more_vert.svg';
+import UnsubscribeIcon from '@/public/images/icon/unsubscribe.svg';
+import WhiteCheckIcon from '@/public/images/icon/white_check.svg';
+import VisibleEyeIcon from '@/public/images/icon/visible_eye.svg';
+import VioletPlusIcon from '@/public/images/icon/violet_plus.svg';
+import DeleteIcon from '@/public/images/icon/delete.svg';
 import NotFound from '@/public/images/icon/404Icon.svg';
 import RightArrow from '@/public/images/icon/arrow-right.svg';
-import ArrowBackwardIcon from '@/public/images/icon/arrow_backward.svg';
-import ArrowDropDownIcon from '@/public/images/icon/arrow_drop_down.svg';
-import ArrowForwardIcon from '@/public/images/icon/arrow_forward.svg';
-import CalendarTodayIcon from '@/public/images/icon/calendar_today.svg';
-import CheckIcon from '@/public/images/icon/check.svg';
-import CloseIcon from '@/public/images/icon/close.svg';
-import CodeitIcon from '@/public/images/icon/codeit.svg';
-import CrownIcon from '@/public/images/icon/crown.svg';
-import DeleteIcon from '@/public/images/icon/delete.svg';
-import EditIcon from '@/public/images/icon/edit.svg';
-import EllipseIcon from '@/public/images/icon/ellipse.svg';
-import EmailIcon from '@/public/images/icon/email.svg';
-import FacebookIconIcon from '@/public/images/icon/facebook_icon.svg';
-import HiddenEyeIcon from '@/public/images/icon/hidden_eye.svg';
-import InstagramIconIcon from '@/public/images/icon/instagram_icon.svg';
-import MoreVertIcon from '@/public/images/icon/more_vert.svg';
-import PlusIcon from '@/public/images/icon/plus.svg';
-import PlusBlueIcon from '@/public/images/icon/plus_blue.svg';
-import PlusVioletIcon from '@/public/images/icon/plus_violet.svg';
-import SearchIcon from '@/public/images/icon/search.svg';
-import SettingIcon from '@/public/images/icon/setting.svg';
-import UnsubscribeIcon from '@/public/images/icon/unsubscribe.svg';
-import VioletPlusIcon from '@/public/images/icon/violet_plus.svg';
-import VisibleEyeIcon from '@/public/images/icon/visible_eye.svg';
-import WhiteCheckIcon from '@/public/images/icon/white_check.svg';
 
 export {
   ArrowBackwardIcon,
@@ -56,7 +56,19 @@ export {
   RightArrow,
 };
 
+// Image
+
 import DEFAULTPROFILEIMAGE from '@/public/images/image/default_profile_image.jpg';
 import DESKTOPIMAGE from '@/public/images/image/desktop.jpg';
+import LANDINGIMAGE1  from '@/public/images/image/landing1.jpg';
+import LANDINGIMAGE2 from '@/public/images/image/landing2.jpg';
+import LANDINGIMAGE3 from '@/public/images/image/landing3.jpg';
+import LANDINGIMAGE4 from '@/public/images/image/landing4.jpg';
+import LANDINGIMAGE5 from '@/public/images/image/landing5.jpg';
 
-export { DEFAULTPROFILEIMAGE, DESKTOPIMAGE };
+export { DEFAULTPROFILEIMAGE, DESKTOPIMAGE, LANDINGIMAGE1,LANDINGIMAGE2,LANDINGIMAGE3,LANDINGIMAGE4,LANDINGIMAGE5 };
+
+// Logo
+import LOGO from '@/public/images/logo/logo_large.jpg';
+
+export {LOGO};
