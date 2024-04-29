02b2b0b7a5d681477f306d1729665ea9744af18b
Taeuk471 <asky990223@gmail.com>
2024-04-30 01:51:59 +0900

    :recycle: Refactor : change modal function

--- utils/api/getDashboardMember.ts
+++ utils/api/getDashboardMember.ts
@@ -1,6 +1,6 @@
 import { getAccessToken } from '../handleToken';
 
-export const getDashboardMember = async ({ dashboard }: { dashboard: string }) => {
+export const getDashboardMember = async ({ dashboard }: { dashboard: number | string }) => {
   const accessToken = getAccessToken();
   const response = await fetch(
     `https://sp-taskify-api.vercel.app/4-14/members?page=1&size=20&dashboardId=${dashboard}`,
