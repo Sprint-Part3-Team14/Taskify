664637cd00c4e9666d808995fba7a081b3645167
KKH <gyooheon@naver.com>
2024-04-29 19:59:08 +0900

    💄 Style : delete margin
    불필요한 마진삭제

--- app/(dashboard)/dashboard/[id]/page.tsx
+++ app/(dashboard)/dashboard/[id]/page.tsx
@@ -141,7 +141,7 @@ const Dashboard = () => {
           )}
         </Droppable>
       </DragDropContext>
-      <div className='mb:sticky mb:py-5 mb:bottom-0 min-w-[20rem] my-11 py-16 px-5 bg-tp-gray_500'>
+      <div className='mb:sticky mb:py-5 mb:bottom-0 min-w-[20rem]  pc:static pc:py-[4rem] px-5 bg-tp-gray_500'>
         <AddButton onClick={handleToggleModal}>{BUTTON.ADD_COLUMN}</AddButton>
         {isShowModal && (
           <CreateColumnModal
